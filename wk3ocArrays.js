b1c5e505d8a84f030b4f6565edbc30a13e1fa5b1
Cmendence <chris.mendence@gmail.com>
2023-02-15 07:08:06 -0500

    Added completed wk3ocArrays.js

---
+++ wk3ocArrays.js
@@ -0,0 +1,159 @@
+// QUESTION 1
+
+// Remove an Element from the Student List Array:
+// Given the array studentList, remove the name "Eddie".
+
+// Do not edit (or hardcode the change to) the array
+//  in its current state. You must edit by using a method of the Array object.
+// Note: arrays are mutable in JavaScript, so you can 
+// use a method to add a new name to an array.
+
+// let studentList = ["Allen", "Jessica", "Eddie", "Sarah"];
+
+// studentList.splice(2,1);
+
+// console.log(studentList)
+
+
+//QUESTION 2
+
+// Write a Function to Sort an Array:
+// Write a function called sortArray that accepts an array 
+// as an argument. Within the function, sort the array 
+// alphabetically or numerically. Return the sorted array.
+
+// function sortArray(var1){
+//     var1.sort();
+//     return var1;
+// }
+
+// also expressed
+
+// function sortArray(array){
+//     return array.sort()
+// }
+
+
+//QUESTION 3
+
+// Array Fun:
+// Using the given function, gradList, add the following 
+// names to the passed array: "Mary", "Steven", "Eddie". 
+// Then, sort the remaining students from Z-A, and return the modified array.
+
+// The passed array is the graduates variable. First add 
+// the names to the graduates array using push. Then sort 
+// the names and finally return the reversed array.
+
+// function gradList(graduates){
+//     //write your code here
+//     graduates.push('Mary', 'Steven', 'Eddie');
+//     graduates.sort();
+//     return graduates.reverse();
+// }
+
+
+// QUESTION 4
+
+// Grocery List Array:
+// Create an array called groceryList and add "granola", "milk", and "eggs"
+
+// let groceryList = ['granola', 'milk', 'eggs'];
+
+
+// QUESTION 5
+
+// Replace an Element of An Array:
+
+// Given the attendanceList array,
+// replace the first element in the array with "Steven".
+
+// let attendanceList = ['Two', 'Two', 'Three'];
+
+// attendanceList.splice(0,1, 'One');
+
+
+// attendanceList.splice(0, 1, 'Steven');
+
+// INSTRUCTOR SOLUTION:
+
+// attendanceList[0] = "Steven";
+
+
+// console.log(attendanceList)
+
+
+
+// QUESTION 6
+
+// Write a Function to Reverse an Array:
+// Write a function called reverseArray that 
+// accepts an array as an argument. Within the 
+// function, reverse the array. Return the reversed array.
+
+// You must use a method of the Array object.
+
+// input = [1, 2, 3, 4, 5];
+
+
+// function reverseArray(array){
+//     array.reverse();
+//     return array;
+// }
+
+// INSTRUCTOR SOLUTION
+
+// function reverseArray(array){
+//     return array.reverse();
+// }
+
+
+
+// QUESTION 7
+
+// Add an Element to the Student List Array:
+// Given the array studentList, add the name "Mary".
+
+// Do not edit (or hardcode the change to) the array 
+// in its current state. You must edit by using a method of the Array object.
+// Note: arrays are mutable in JavaScript, so you can 
+// use a method to add a new name to an array.
+
+// 
+
+
+//QUESTION 8
+
+
+// Write a Function to Join Two Arrays:
+// Write a function called joinArrays that 
+// accepts two arrays as arguments, joins 
+// the two arrays and returns the new array.
+
+// You must use a method of the Array object
+
+// function joinArrays(arr1, arr2){
+//     return arr1.concat(arr2);
+// }
+
+// Write a Function to Print the Elements of an Array:
+// Write a function called printArray that accepts 
+// an array as an argument, and contains a loop that 
+// will print each value of the passed in array.
+
+// Do not edit (or hardcode the change to) the array 
+// in its current state. You must edit by using a method of the Array object.
+// Note: arrays are mutable in JavaScript, so you can 
+// use a method to add a new name to an array.
+
+// let array = [1, 2, 3, 4, 5];
+
+// function printArray(arr1){
+//     for (let i = 0; i < arr1.length; i++)
+//     console.log(arr1[i]);
+// }
+
+
+// printArray(array)
+
+
