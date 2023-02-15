188e4a285d5848a6cf85ed327c00325c7c38ffbb
Cmendence <chris.mendence@gmail.com>
2023-02-14 22:47:57 -0500

    added wk3 Open Class completed

---
+++ wk3ocFunctions.js
@@ -0,0 +1,122 @@
+//QUESTION 1
+
+// Function isBlue:
+// Create a function named isBlue. isBlue will take one parameter, 
+// and it will return true if it is passed "blue", otherwise it will return false.
+
+// The task here is to write the function isBlue.
+
+// function isBlue(color) {
+//     if (color.toLowerCase() === "blue") {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   }
+
+
+// Function loopUntilX:
+// Write a function named loopUntilX that takes one parameter.
+//  Within the function, write a loop that loops as many times 
+//  as the passed in value, starting at 0. Within the loop, 
+//  print the number of iterations to the console. Once the loop is done, return true.
+
+// The task here is to write the function loopUntilX.
+
+
+// QUESTION 2
+//
+// function loopUntilX(num){
+//     for (let i = 0; i < num; i++){
+//         console.log(i)
+//     } return true
+// }
+
+// loopUntilX(4);
+
+
+
+//QUESTION 3
+
+
+// Function findCircumference:
+// Create a function named findCircumference that 
+// returns the circumference of a circle if given the radius.
+
+// function findCircumference(radius){
+//     return (Math.PI * (radius*2))
+// }
+
+
+
+//QUESTION 4
+
+// Function returnSum:
+// Create a function named returnSum that accepts 
+// 3 integer parameters named num1, num2, and num3 
+// and returns the sum of the three parameters.
+
+// The task here is to write the function returnSum.
+
+
+// function returnSum(num1, num2, num3){
+//     return num1 + num2 + num3;
+// }
+
+
+
+//QUESTION 5
+
+// Function checkout:
+// Create a function named checkout that accepts 2 
+//  named cardBalance and price. If the price is more 
+//  than the card balance, return false; otherwise, return the new card balance.
+
+// The task here is to write the function checkout.
+
+// function checkout(cardBalance, price){
+//     if(price > cardBalance){
+//          return false;
+//     }else {
+//         return cardBalance-=price;
+//     }   
+// }
+
+// console.log(checkout(10,2));
+
+
+//QUESTION 6
+
+// Function speakFriend:
+// Write a function named speakFriend that takes one parameter. 
+// Use console.log() to print the requested information to the console.
+
+// Return the string "Access Denied" Except if the passed 
+// in value is equal to "Mellon", then return "Enter"
+
+// function speakFriend(name1){
+//     console.log(name1)
+//     if(name1 === 'Mellon'){
+//         return 'Enter'
+//     } else {
+//     return 'Access Denied';
+// }
+// } 
+
+// console.log(speakFriend(''))
+
+
+//QUESTION 7
+
+// Function sumOfTwo:
+// Write a function named sumOfTwo that takes two numbers
+//  as parameters and returns the sum. The variables 
+//  num1 and num2 are declared and initialized, and used in the call to sumOfTwo.
+
+// The task here is to write the function sumOfTwo.
+
+// function sumOfTwo(num1, num2){
+//     return num1 + num2;
+// }
+
+// console.log(sumOfTwo(3,2))
